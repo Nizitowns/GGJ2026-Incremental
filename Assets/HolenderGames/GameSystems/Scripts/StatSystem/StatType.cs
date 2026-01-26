cff919e8f90b6253cdac41529320c881559fd834
Antony Nizamoglou <a.nizamoglou@hotmail.com>
2026-01-26 11:27:02 +0200

    First Commit

---
+++ Assets/HolenderGames/GameSystems/Scripts/StatSystem/StatType.cs
@@ -0,0 +1,25 @@
+
+namespace HolenderGames.StatSystem
+{
+    /// <summary>
+    /// All the types of stats that upgrades can manipulate
+    /// Don't change the order, as it will break existing upgrades
+    /// </summary>
+    public enum StatType
+    {
+        Wizard_Damage,
+        Wizard_AttackSpeed,
+        Wizard_Shields,
+        Wizard_Health,
+        Magician_Damage,
+        Magician_AttackSpeed,
+        Magician_Shields,
+        Magician_Health,
+        UnlockMagician,
+        UnlockQuestBoard,
+        Test123
+
+    }
+
+}
+
